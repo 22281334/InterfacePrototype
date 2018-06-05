457e88916582bb41ad348602fe82af86f47d3dab
Remnantww <jopoi1999@gmail.com>
2018-06-05 21:20:24 +0930

    cleaning

--- src/CleaningStage.java
+++ src/CleaningStage.java
@@ -1,5 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -9,8 +12,14 @@ public class CleaningStage {
         Stage mainStage = new Stage();
         mainStage.setX(positionX);
         mainStage.setY(positionY);
-        GridPane mainPane = new GridPane();
-        Scene scene = new Scene(mainPane,sizeX,sizeY);
+
+
+        Image roomPic = new Image(getClass().getResourceAsStream("room.png"));
+
+        Button room = new Button("Room",new ImageView(roomPic));
+        GridPane cleaningLayout = new GridPane();
+        cleaningLayout.add(room,0,0);
+        Scene scene = new Scene(cleaningLayout,sizeX,sizeY);
 
         mainStage.setScene(scene);
         mainStage.show();
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main extends Application{
 
     public Main(){
         //HomeStage home = new HomeStage(300,300,500,500);
-        //CleaningStage cleaningStage = new CleaningStage(300,300,500,500);
+        CleaningStage cleaningStage = new CleaningStage(300,300,500,500);
     }
 
     public static void main(String[] args) {
