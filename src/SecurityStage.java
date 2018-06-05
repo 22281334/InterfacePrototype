9f9b0e0d8b36cb3bb3b84b5f2d67a5f3f03edd7d
liudayun <22281334@qq.com>
2018-06-05 14:10:46 +0930

    Initial commit

---
+++ src/SecurityStage.java
@@ -0,0 +1,27 @@
+import javafx.stage.Stage;
+
+public class SecurityStage {
+
+
+    double sizeX;
+    double sizeY;
+    double positionX;
+    double positionY;
+    Stage stage = new Stage();
+
+    public SecurityStage(double sizeX,double sizeY,double positionX,double positionY){
+        this.sizeX=sizeX;
+        this.sizeY=sizeY;
+        this.positionX=positionX;
+        this.positionY=positionY;
+
+
+
+
+
+
+
+
+
+    }
+}
