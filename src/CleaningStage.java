67a652dde4ce8924ddd32d36d28f0c4fa034119e
Remnantww <jopoi1999@gmail.com>
2018-06-05 22:03:36 +0930

    cleaning

--- src/CleaningStage.java
+++ src/CleaningStage.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -13,14 +14,24 @@ public class CleaningStage {
         mainStage.setX(positionX);
         mainStage.setY(positionY);
 
+        BorderPane content = new BorderPane();
+        GridPane cleaningLayout = new GridPane();
+        GridPane topLayout = new GridPane();
 
         Image roomPic = new Image(getClass().getResourceAsStream("room.png"));
-
+        Image homePic = new Image(getClass().getResourceAsStream("Home.png"));
         Button room = new Button("Room",new ImageView(roomPic));
-        GridPane cleaningLayout = new GridPane();
-        cleaningLayout.add(room,0,0);
-        Scene scene = new Scene(cleaningLayout,sizeX,sizeY);
+        Button home = new Button("",new ImageView(homePic));
 
+        room.setStyle("-fx-font-size: 20;");
+
+
+        cleaningLayout.add(room,0,0);
+        topLayout.add(home,0,0);
+        content.setTop(topLayout);
+        content.setCenter(cleaningLayout);
+
+        Scene scene = new Scene(content,sizeX,sizeY);
         mainStage.setScene(scene);
         mainStage.show();
     }
