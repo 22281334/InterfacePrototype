e5f53ba15c6f6dba5df68b5a427067e508828293
liudayun <22281334@qq.com>
2018-06-07 23:41:04 +0930

    Security Icon added

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,15 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-
-    public Main(){
-        //HomeStage home = new HomeStage(300,300,500,500);
-        CleaningStage cleaningStage = new CleaningStage(400,100,1000,800);
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //CleaningStage cleaningStage = new CleaningStage(400,100,1000,800);
+        //HomeStage home = new HomeStage(300,300,500,500);
 
+        SecurityStage securityStage=new SecurityStage(500,500,0,0);
     }
 }
--- src/SecurityStage.java
+++ src/SecurityStage.java
@@ -1,3 +1,8 @@
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class SecurityStage {
@@ -14,14 +19,25 @@ public class SecurityStage {
         this.sizeY=sizeY;
         this.positionX=positionX;
         this.positionY=positionY;
+        BorderPane content = new BorderPane();
+
+        stage.setX(positionX + 20);
+        stage.setY(positionY);
+
+        GridPane gridPane = new GridPane();
+        Image image = new Image("pic/SecurityIcon.png");
+
+        ImageView imageView = new ImageView(image);
+        gridPane.add(imageView,0,0);
+        content.setCenter(gridPane);
 
 
+        stage.setTitle("Security");
 
+        //start the window.
+        Scene scene = new Scene(content, sizeX, sizeY);
 
-
-
-
-
-
+        stage.setScene(scene);
+        stage.show();
     }
 }
